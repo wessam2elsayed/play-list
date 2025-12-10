317f841d3ab016eb78b59113b84c4a43d1a393b9
Wessam <wessam2elsayed82@gmail.com>
2025-12-11 01:32:01 +0200

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:play_list/home/screens/try_screen.dart';
-
-
-
-
+import 'package:play_list/home/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TryScreen(),
+      home: HomeScreen(),
     );
   }
 }
